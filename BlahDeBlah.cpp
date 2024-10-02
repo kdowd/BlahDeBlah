c101563f36da09d8c04d64219670c6c38e2c793f
091275 <091275@CIT3-205-02>
2024-10-03 10:30:01 +1300

    Add project files.

---
+++ BlahDeBlah.cpp
@@ -0,0 +1,12 @@
+// BlahDeBlah.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World2!\n";
+
+
+}
+
